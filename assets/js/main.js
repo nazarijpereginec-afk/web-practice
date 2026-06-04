4acd3daaf6c3f14e4fb9588452c568b2aabf3f5c
Nazariy Perehinets <nazarijpereginec@gmail.com>
2026-06-04 14:56:55 +0300

    Add header title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-const iconOpen = getElementById
+const iconClose = document.getElementById('iconClose');
+const iconOpen = document.getElementById('iconOpen');
+const mainMenu = document.getElementById('mainMenu');
+const mainHeader = document.getElementById('mainHeader');
+
+iconOpen.addEventListener('click', () => {
+    mainMenu.classList.toggle('menu-open');
+    mainHeader.classList.toggle('d-none');
+})
+
+iconClose.addEventListener('click', () => {
+    mainMenu.classList.remove('menu-open');
+    mainHeader.classList.remove('d-none');
+})
+
+
