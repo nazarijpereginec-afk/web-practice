50b2e0fea047f245bf87708f5c5cf04c38d3c468
Nazariy Perehinets <nazarijpereginec@gmail.com>
2026-06-11 15:57:04 +0300

    Add menu categories section and update event list in java script

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,18 @@ const iconClose = document.getElementById('iconClose');
 const iconOpen = document.getElementById('iconOpen');
 const mainMenu = document.getElementById('mainMenu');
 const mainHeader = document.getElementById('mainHeader');
+const main = document.getElementById('main');
 
 iconOpen.addEventListener('click', () => {
     mainMenu.classList.toggle('menu-open');
     mainHeader.classList.toggle('d-none');
+    main.classList.toggle('d-none');
 })
 
 iconClose.addEventListener('click', () => {
     mainMenu.classList.remove('menu-open');
     mainHeader.classList.remove('d-none');
+    main.classList.remove('d-none');
 })
 
 let viewportWidth = window.innerWidth;
