74ee97763f150e0e730135d06e2978661c417fd6
Nazariy Perehinets <nazarijpereginec@gmail.com>
2026-06-13 19:08:31 +0300

    Add footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,19 @@ const iconOpen = document.getElementById('iconOpen');
 const mainMenu = document.getElementById('mainMenu');
 const mainHeader = document.getElementById('mainHeader');
 const main = document.getElementById('main');
+const footer = document.getElementById('footer');
+
 
 iconOpen.addEventListener('click', () => {
     mainMenu.classList.toggle('menu-open');
     mainHeader.classList.toggle('d-none');
     main.classList.toggle('d-none');
+    footer.classList.toggle('d-none');
 })
 
 iconClose.addEventListener('click', () => {
     mainMenu.classList.remove('menu-open');
     mainHeader.classList.remove('d-none');
     main.classList.remove('d-none');
+    footer.classList.remove('d-none');
 })
-
-let viewportWidth = window.innerWidth;
-console.log("Ширина вікна браузера: " + viewportWidth);
