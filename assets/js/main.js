0a5f14fefa8123cc0fb5e0ca489f43ab1823036f
Nazariy Perehinets <nazarijpereginec@gmail.com>
2026-06-14 20:47:00 +0300

    Maybe full complet practic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/* burger menu */
 const iconClose = document.getElementById('iconClose');
 const iconOpen = document.getElementById('iconOpen');
 const mainMenu = document.getElementById('mainMenu');
@@ -5,14 +6,12 @@ const mainHeader = document.getElementById('mainHeader');
 const main = document.getElementById('main');
 const footer = document.getElementById('footer');
 
-
 iconOpen.addEventListener('click', () => {
     mainMenu.classList.toggle('menu-open');
     mainHeader.classList.toggle('d-none');
     main.classList.toggle('d-none');
     footer.classList.toggle('d-none');
 })
-
 iconClose.addEventListener('click', () => {
     mainMenu.classList.remove('menu-open');
     mainHeader.classList.remove('d-none');
@@ -20,10 +19,10 @@ iconClose.addEventListener('click', () => {
     footer.classList.remove('d-none');
 })
 
+/* modal window */
 const btnReservation = document.getElementById('btnReservation');
 const closeModal = document.getElementById('closeModal');
 const modal = document.getElementById('modal');
-
 btnReservation.addEventListener('click', () => {
     modal.classList.remove('d-none');
 })
@@ -31,3 +30,18 @@ btnReservation.addEventListener('click', () => {
 closeModal.addEventListener('click', () => {
     modal.classList.add('d-none');
 })
+
+/* block animation */
+const reveals = document.querySelectorAll('.reveal');
+window.addEventListener('scroll', reveal);
+function reveal() {
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const visible = 150;
+
+        if (elementTop < windowHeight - visible) {
+            element.classList.add('active');
+        }
+    });
+}
